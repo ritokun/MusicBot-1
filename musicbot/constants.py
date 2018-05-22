57383cdf95afda6a0b9e481973e4ee223486d74e
kosugikun <kosukisaki@yahoo.co.jp>
2018-05-22 21:48:26 +0900

    バージョン変更

--- musicbot/constants.py
+++ musicbot/constants.py
@@ -1,7 +1,7 @@
 import os.path
 
 MAIN_VERSION = '1.1.0'
-SUB_VERSION = 'alpha.1'
+SUB_VERSION = 'alpha.2'
 VERSION = MAIN_VERSION + SUB_VERSION
 
 AUDIO_CACHE_PATH = os.path.join(os.getcwd(), 'audio_cache')
