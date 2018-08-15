5087e9ba3b21780a8e83ae42e9dd1894b57d4f24
kosugikun <kosukisaki@yahoo.co.jp>
2018-08-15 18:52:48 +0900

    細かな修正

--- musicbot/constants.py
+++ musicbot/constants.py
@@ -1,13 +1,7 @@
 import os.path
 
-<<<<<<< HEAD
 MAIN_VERSION = '1.1.0'
 SUB_VERSION = 'by Kosugi_kun'
-=======
-MAIN_VERSION = '1.0.2'
-SUB_VERSION = 'build.5'
-
->>>>>>> 63e9e292756568775b5e331283361dcc563666bb
 VERSION = MAIN_VERSION + SUB_VERSION
 
 AUDIO_CACHE_PATH = os.path.join(os.getcwd(), 'audio_cache')
